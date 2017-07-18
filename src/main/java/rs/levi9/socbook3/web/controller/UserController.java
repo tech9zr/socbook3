a0e6eb06f4101cf7c0f560b76f2aafd8dd824cc9
kurs <kurs@JAVAKURS06.levi9.com>
2017-07-18 12:29:12 +0200

    added user controller

---
+++ src/main/java/rs/levi9/socbook3/web/controller/UserController.java
@@ -0,0 +1,29 @@
+package rs.levi9.socbook3.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import rs.levi9.socbook3.domain.User;
+import rs.levi9.socbook3.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public User save(@Valid @RequestBody User user) {
+		return userService.save(user);
+	}
+}
