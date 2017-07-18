206c84aaae0a8ba696a07612d3155d8ba638a1c4
kurs <kurs@JAVAKURS04.levi9.com>
2017-07-18 09:47:50 +0200

    Ovo je pocetaK

--- src/main/java/rs/levi9/socbook3/Socbook3Application.java
+++ src/main/java/rs/levi9/socbook3/Socbook3Application.java
@@ -1,8 +1,13 @@
 package rs.levi9.socbook3;
  
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Socbook3Application {
 
 	public static void main(String[] args) {
+		 SpringApplication.run(Socbook3Application.class, args);
 	}
 
 }
