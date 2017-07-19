d7149ac253a56af7463d2861c64059c1edd90d4c
ACurguz <aleksandracurguz@gmail.com>
2017-07-19 13:16:46 +0200

    spring security

--- src/main/java/rs/levi9/socbook3/Socbook3Application.java
+++ src/main/java/rs/levi9/socbook3/Socbook3Application.java
@@ -2,8 +2,10 @@ package rs.levi9.socbook3;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Socbook3Application
 {
 
--- src/main/java/rs/levi9/socbook3/repository/UserRepository.java
+++ src/main/java/rs/levi9/socbook3/repository/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import rs.levi9.socbook3.domain.User;
 @Repository
-public interface UserRepository  extends JpaRepository<User, Long>{
+public interface UserRepository  extends JpaRepository<User, Long>
+{
 	
-public User findByUsername(String username);
+      public User findByUsername(String username);
 	
 
 }
