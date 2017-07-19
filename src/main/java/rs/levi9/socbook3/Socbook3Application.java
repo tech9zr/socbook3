e4527847d5aba1f96afec9bf1b46827991941f09
Dejan Ivanovic <ivanovic.dejan@gmail.com>
2017-07-19 13:20:26 +0200

    fix

--- src/main/java/rs/levi9/socbook3/Socbook3Application.java
+++ src/main/java/rs/levi9/socbook3/Socbook3Application.java
@@ -2,7 +2,9 @@ package rs.levi9.socbook3;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableGlobalMethodSecurity(prePostEnabled =true)
 @SpringBootApplication
 public class Socbook3Application
 {
