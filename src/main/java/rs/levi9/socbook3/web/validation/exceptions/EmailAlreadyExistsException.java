430e54e95c33ce8ae76c3b2b873fca9089319cf9
Dejan Ivanovic <ivanovic.dejan@gmail.com>
2017-07-20 13:53:31 +0200

    validation fix

---
+++ src/main/java/rs/levi9/socbook3/web/validation/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package rs.levi9.socbook3.web.validation.exceptions;
+
+public class EmailAlreadyExistsException extends Exception {
+	
+	public EmailAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/rs/levi9/socbook3/web/validation/exceptions/UsernameAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package rs.levi9.socbook3.web.validation.exceptions;
+
+public class UsernameAlreadyExistsException extends Exception {
+	
+	public UsernameAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
